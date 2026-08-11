726ae97dad87118d9de4e14f9a3720b9a46bff74
suman verma <sumanverma26102004@gmail.com>
2026-08-11 14:00:20 +0530

    problem Table Entity

---
+++ src/main/java/com/Codexa/Codexa/repository/ProblemRepository.java
@@ -0,0 +1,7 @@
+ package com.Codexa.Codexa.repository;
+
+import com.Codexa.Codexa.entity.Problem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProblemRepository extends JpaRepository<Problem, Long> {
+}
