bcce67d4fcedfcb9377109c1a8c06201f8aa7b33
suman verma <sumanverma26102004@gmail.com>
2026-08-23 12:07:56 +0530

    problem page added

--- src/main/java/com/Codexa/Codexa/controller/ProblemController.java
+++ src/main/java/com/Codexa/Codexa/controller/ProblemController.java
@@ -4,10 +4,9 @@ import com.Codexa.Codexa.dto.CreateProblemRequest;
 import com.Codexa.Codexa.entity.Problem;
 import com.Codexa.Codexa.service.ProblemService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/problems")
@@ -19,6 +18,7 @@ public class ProblemController {
         this.problemService = problemService;
     }
 
+    // Create problem
     @PostMapping
     public ResponseEntity<Problem> createProblem(
             @Valid @RequestBody CreateProblemRequest request) {
@@ -28,14 +28,41 @@ public class ProblemController {
         );
     }
 
+    // Get problems with pagination, search, filter and sorting
     @GetMapping
-    public ResponseEntity<List<Problem>> getAllProblems() {
+    public ResponseEntity<Page<Problem>> getAllProblems(
+
+            @RequestParam(defaultValue = "0")
+            int page,
+
+            @RequestParam(defaultValue = "10")
+            int size,
+
+            @RequestParam(required = false)
+            String search,
+
+            @RequestParam(required = false)
+            String difficulty,
+
+            @RequestParam(defaultValue = "id")
+            String sortBy,
+
+            @RequestParam(defaultValue = "asc")
+            String direction) {
 
         return ResponseEntity.ok(
-                problemService.getAllProblems()
+                problemService.searchProblems(
+                        search,
+                        difficulty,
+                        page,
+                        size,
+                        sortBy,
+                        direction
+                )
         );
     }
 
+    // Get problem by ID
     @GetMapping("/{id}")
     public ResponseEntity<Problem> getProblemById(
             @PathVariable Long id) {
